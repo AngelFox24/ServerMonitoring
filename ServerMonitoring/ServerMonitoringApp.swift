7ef05b59b282ef4e4358f85d378e2bf16ac2d42d
AngelFox24 <curilaurente@gmail.com>
2024-05-25 14:08:50 -0500

    Initial Commit

---
+++ ServerMonitoring/ServerMonitoringApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServerMonitoringApp.swift
+//  ServerMonitoring
+//
+//  Created by Angel Curi Laurente on 25/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ServerMonitoringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
